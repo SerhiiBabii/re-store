745411ed2a7f5eaa45afea7be43bafa0f95166f7
Serhii Babii <babiysergey1988@gmail.com>
2019-10-06 19:01:50 +0300

    Modified main index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Hello");
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./components/app";
+import ErrorBoudry from "./components/error-boundry";
+import BookstoreService from "./services/bookstore-service";
+import { BookstoreServiceProvider } from "./components/bookstore-service-context";
+
+import store from "./store";
+
+const bookstoreService = new BookstoreService();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoudry>
+      <BookstoreServiceProvider value={bookstoreService}>
+        <Router>
+          <App />
+        </Router>
+      </BookstoreServiceProvider>
+    </ErrorBoudry>
+  </Provider>,
+  document.getElementById("root")
+);
