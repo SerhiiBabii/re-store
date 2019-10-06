3fd401761407771c526231c1d83310d1158119dc
Serhii Babii <babiysergey1988@gmail.com>
2019-10-06 19:05:34 +0300

    Modified component ErrorIndicator

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./error-indicator.css";
+
+const ErrorIndicator = () => {
+  return (
+    <div>
+      <span>Something has gone terribly wrong</span>
+      <span>(but we already sent men to fix it)</span>
+    </div>
+  );
+};
+
+export default ErrorIndicator;
