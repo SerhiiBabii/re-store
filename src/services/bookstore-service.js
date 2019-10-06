fea09f737cf4c1fe7bb74c4d1512d4abdd92fe96
Serhii Babii <babiysergey1988@gmail.com>
2019-10-06 19:09:24 +0300

    Modified BookstoreService

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,5 +1,47 @@
 export default class BookstoreService {
-  getBook() {
-    return [];
+  data = [
+    {
+      id: 1,
+      title: "123",
+      author: "Some Name1",
+      price: 120,
+      coverImage:
+        "https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg"
+    },
+    {
+      id: 2,
+      title: "234",
+      author: "Some Name2",
+      price: 80,
+      coverImage:
+        "https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg"
+    },
+    {
+      id: 3,
+      title: "345",
+      author: "Some Name3",
+      price: 20,
+      coverImage:
+        "https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg"
+    },
+    {
+      id: 4,
+      title: "456",
+      author: "Some Name4",
+      price: 68,
+      coverImage:
+        "https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg"
+    }
+  ];
+  getBooks() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Math.random() > 0.75) {
+          resolve(this.data);
+        } else {
+          reject(new Error("Somethis bad happen!"));
+        }
+      }, 1000);
+    });
   }
 }
