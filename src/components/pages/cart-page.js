da108e41686de86e5a6355c0befe4237c1b7dda1
Serhii Babii <babiysergey1988@gmail.com>
2019-10-10 14:53:08 +0300

    Added ShoppingCartTable-component in Cart-page

--- src/components/pages/cart-page.js
+++ src/components/pages/cart-page.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ShoppingCartTable from "../shopping-cart-table";
 
 const CartPage = () => {
   return (
     <div>
       <h2>Card page</h2>
+      <ShoppingCartTable isImage={true} />
     </div>
   );
 };
